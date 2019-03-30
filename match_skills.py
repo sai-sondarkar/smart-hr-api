a805bd66a6addb70b2bdc45b9dcbb63a2f38e42a
sa--sondarkar <saisondarkar18@gmail.com>
2019-03-30 22:28:50 +0530

    make it better

---
+++ match_skills.py
@@ -0,0 +1,21 @@
+
+from fuzzywuzzy import fuzz
+from flask import Flask, abort, jsonify, request
+
+
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def match_competency():
+    data = request.get_json(force=True)
+    
+    num = fuzz.ratio('skilla skillb skillc','skilla skillb')
+    output = {'match': int(num)}
+    print(output)
+    return jsonify(results=output)
+
+if __name__ == '__main__':
+    app.run()
+
+    
